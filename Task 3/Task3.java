faaa26ba43103fc08b569d91255ef4d22ce7c751
SOUTaccount <75136887+SOUTaccount@users.noreply.github.com>
2021-10-13 16:48:44 +0300

    Add Task3
    Выведите на экран фразу "Программировать - это легко! " 5 раз,  каждый раз через пробел.  
    Sample Input:  
    Sample Output:  
    Программировать - это легко! Программировать - это легко! Программировать - это легко! Программировать - это легко! Программировать - это легко!

---
+++ Task 3/Task3.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Программировать - это легко! Программировать - это легко! Программировать - это легко! Программировать - это легко! Программировать - это легко!");
+    }
+}
