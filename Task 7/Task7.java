6d2070c73cea1eb56b211de10087291be59ce9e9
SOUTaccount <75136887+SOUTaccount@users.noreply.github.com>
2021-10-15 22:23:14 +0300

    Add Task7
    Вводится буква, необходимо вывести ее код.  
    Sample Input 1:  
    a  
    Sample Output 1:  
    97

---
+++ Task 7/Task7.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Task {
+    public static void main(String[] args) throws IOException {
+        int c1=System.in.read();
+        System.out.print(c1);
+    }
+}
