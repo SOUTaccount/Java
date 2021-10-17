c718c3cc07613a8949670c4cc1d3e5a58ef06203
Stebakov Vladimir <okmda@yandex.ru>
2021-10-17 23:15:31 +0300

    Add Task8
    ﻿Вводятся две букв необходимо вывести сумму кодов этих букв  
    Sample Input:  
    a b  
    Sample Output:  
    195

---
+++ Task 8/Task8.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Task {
+    public static void main(String[] args) throws IOException {
+        int c1=System.in.read();
+        int c2=System.in.read();
+        int c3=System.in.read();
+        System.out.print(c3+c1);
+    }
+}
