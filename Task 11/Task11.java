7eb6e2b21ec8d26960cf45ef455c0245faedd3a6
Stebakov Vladimir <okmda@yandex.ru>
2021-10-19 18:04:11 +0300

    Add Task11
    Сколько раз из первого числа можно вычесть второе число?  
    Sample Input:  
    5 1  
    Sample Output:  
    5

---
+++ Task 11/Task11.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Task {
+    public static void main(String[] args) throws IOException {
+        Scanner e= new Scanner(System.in);
+        int f = e.nextInt();
+        int b = e.nextInt();
+        int g = f/b;
+        System.out.print(g);
+    }
+}
