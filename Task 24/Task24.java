3c96c84df49e3b41a2107bd3a0465d3c21cf20fa
Stebakov Vladimir <okmda@yandex.ru>
2021-10-27 21:47:10 +0300

    Add Task24
    Срезом будем считать последовательность подряд идущих элементов массива, где каждый следующий элемент больше предыдущего.  
    Для последовательности 1 2 3 1 2 5 7 1 2 1 2, существуют возрастающие срезы: 1 2 3, 1 2 5 7, 1 2, 1 2 вывести нужно срез 1 2 5 7  
    Для последовательности 1 2 3 1 2 5 1 2 7, существуют срезы 1 2 3, 1 2 5, 1 2 7 вывести нужно все срезы, так как они равной длины  
    Формат входных данных:  
    Дано натуральное число N, далее следуют N целых чисел.  
    Формат выходных данных:  
    В первой строке выведите длину максимального среза  
    Далее выведите содержание среза/срезов, разделяя элементы одним пробелом, каждый срез с новой строки  
    Sample Input 1:  
    7  
    2 1 2 3 1 5 7  
    Sample Output 1:  
    3  
    1 2 3   
    1 5 7

---
+++ Task 24/Task24.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Task {
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+
+        int[] input = new int[N];
+        for(int i=0; i<N; i++) input[i] = in.nextInt();
+
+        int[] lens = new int[input.length];
+
+        int max = 0;
+
+        for(int i=1; i<input.length; i++)
+        {
+            if (input[i]>input[i-1])
+                lens[i] = lens[i-1]+1;
+            else lens[i] = 0;
+            max = Math.max(lens[i], max);
+        }
+
+        System.out.println(max + 1);
+
+        for(int i=0; i<input.length; i++)
+        {
+            if (lens[i] == max)
+            {
+                for(int j=i-lens[i]; j<=i; j++)
+                    System.out.print(input[j] + " ");
+                System.out.println();
+            }
+        }
+    }
+}
