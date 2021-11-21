50905d1c89999ca46b6acb73c6f8e189167a54a8
Stebakov Vladimir <okmda@yandex.ru>
2021-11-21 22:11:15 +0300

    Add Task36
    Реализуйте функцию, возвращающую сумму двух чисел. Менять сигнатуру функции не нужно. Писать что то кроме функции - не нужно! Задумываться о вводе тоже не нужно!  
    Sample Input 1:  
    8 11  
    Sample Output 1:   
    19

---
+++ Task 36/Task36.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Task {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        sum(a,b);
+    }
+    static int sum(int a, int b){
+        return (a+b);
+    }
+    }
