1074efed2878230ca76e0663eac54632aaae1a1f
Stebakov Vladimir <okmda@yandex.ru>
2021-12-10 23:18:50 +0300

    Add Task47
    ﻿Спроектируйте класс с именем Cat, с тремя полями внутри класса: String name, int age, int weight.
    Необходимо написать класс Cat, больше ничего писать не надо!

---
+++ Task 47/Task47.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Task {
+    static class Cat {
+
+        String name = "petya";
+        int age = 6;
+        int weight = 6;
+    }
+}
