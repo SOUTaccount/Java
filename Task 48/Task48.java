834cd8adcd5cace3183d8a7e22e0d9b91849dc5c
Stebakov Vladimir <okmda@yandex.ru>
2021-12-15 19:44:15 +0300

    Add Task48
    ﻿Спроектируйте два класса: Cat, Dog;
    В каждом из классов должно быть по два поля - name, age;

---
+++ Task 48/Task48.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Task {
+    static class Cat{
+        String name;
+        int age;}
+    static class Dog{
+        String name;
+        int age;}
+}
